f11ca989770925c8723774dcc476925c277738c8
Oleksii Kropachov <o.kropachov@shamanland.com>
2015-04-15 23:23:35 +0300

    Use <input> instead window.prompt

--- popup.js
+++ popup.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
   var cells = document.querySelectorAll("td");
   for (var i = 0, n = cells.length; i < n; ++i) {
     cells[i].addEventListener("click", function() {
-      window.prompt("Press Ctrl+C to copy", this.innerText || this.textContent);
+      var input = document.querySelector("input");
+      input.value = this.textContent;
+      input.select();
     });
   }
 });
